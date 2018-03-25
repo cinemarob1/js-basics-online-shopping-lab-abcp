cdb27525bb56c322bcfa274a1f46b4d9758abe09
Robert H Boscacci <cinemarob1@gmail.com>
2018-03-25 10:26:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ function viewCart() {
   } 
   
   else if(cart.length===1){
-    console.log('In your cart, you have ' + cart[0] + ' at ' + cart[0] + ' .')
+    console.log('In your cart, you have ' + cart[0].itemName + ' at ' + cart[0].itemPrice + ' .')
   }
   return;
 }
