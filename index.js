55077dc583f11c8822523119eca11a5af501ecf2
Robert H Boscacci <cinemarob1@gmail.com>
2018-03-25 10:12:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,12 +9,34 @@ function setCart(c) {
   return cart;
 }
 
-function addToCart(item) {
- // write your code here
+function addToCart(newItemName) {
+  
+  //Create new item
+  var newItem = {
+    name: newItemName, 
+    price: Math.floor(Math.random()*100)
+  };
+  
+  //Add new item object to cart array
+  cart.push(newItem);
+  
+  //Print:
+  console.log('${newItemName} has been added to your cart.')
+  
+  return(cart);
+  
 }
 
 function viewCart() {
-  // write your code here
+  
+  if(cart.length===0){
+    console.log('Your shopping cart is empty.')
+  } 
+  
+  else if(cart.length===1){
+    console.log('In your cart, you have ${cart[0].name} at ${cart[0].price}.')
+  }
+  return;
 }
 
 function total() {
